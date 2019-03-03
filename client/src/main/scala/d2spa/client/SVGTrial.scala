e42146d8357e71e4f957246dfb6d4b03d88d805d
domschoen <dschoenenberger@mac.com>
2019-03-03 21:16:29 +0100

    Add svg trial

--- client/src/main/scala/d2spa/client/SVGTrial.scala
+++ client/src/main/scala/d2spa/client/SVGTrial.scala
@@ -12,11 +12,12 @@ import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.vdom.all.key
 import japgolly.scalajs.react.vdom.SvgTags._
 import japgolly.scalajs.react.vdom.SvgAttrs._
+//import japgolly.scalajs.react.vdom.svg_<^._
 
 
 import paths.high.Bar
 
-
+// https://reqres.in/
 
 object SVGTrial {
   case class Stats(values: List[List[Double]], labels: List[String])
@@ -99,19 +100,28 @@ object SVGTrial {
       val groups = stats.values.length
       val middle = groups / 2
       val count = stats.values.head.length
-/*
+
       val rectangles = bar.curves.zipWithIndex map { case (curve, i) =>
         if (curve.index == middle) g(
           path(d := curve.line.path.print, stroke := "none", fill := string(palette(curve.index))),
-          text(transform := below(curve.line.centroid), textAnchor := "middle", stats.labels(i / count))
+          text(transform := below(curve.line.centroid.toArray), textAnchor := "middle", stats.labels(i / count))
         )
         else path(d := curve.line.path.print, stroke := "none", fill := string(palette(curve.index)))
       }
 
-      svg(width := 460, height := 400,
+      /*svg(width := 460, height := 400,
         rectangles
       )*/
-      <.div("")
+      <.div(
+        ^.className := "react-sortable-handle",
+        svg(
+          ^.className := "react-sortable-handle-svg",
+          viewBox := "0 0 24 24",
+          path(d := "M9,8c1.1,0,2-0.9,2-2s-0.9-2-2-2S7,4.9,7,6S7.9,8,9,8z M9,10c-1.1,0-2,0.9-2,2s0.9,2,2,2s2-0.9,2-2S10.1,10,9,10z M9,16c-1.1,0-2,0.9-2,2s0.9,2,2,2s2-0.9,2-2S10.1,16,9,16z"),
+          path(d := "M15,8c1.1,0,2-0.9,2-2s-0.9-2-2-2s-2,0.9-2,2S13.9,8,15,8z M15,10c-1.1,0-2,0.9-2,2s0.9,2,2,2s2-0.9,2-2S16.1,10,15,10z M15,16c-1.1,0-2,0.9-2,2s0.9,2,2,2s2-0.9,2-2S16.1,16,15,16z")
+        )
+      )
+
     }
   }
 
